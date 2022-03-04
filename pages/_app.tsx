2a020f0b116b0c3edab8358e76cfd6485947b93b
Bhanjo <hanjo1515@naver.com>
2022-03-04 21:54:13 +0900

    setting: styled components 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
+import Header from '../components/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
