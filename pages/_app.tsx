d0d3adc1bb4f837c86784d5eceb0d473385e477e
Bhanjo <hanjo1515@naver.com>
2022-07-18 01:10:02 +0900

    feature: 스토어 지정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,23 @@ import GlobalStyle from '../styles/GlobalStyle';
 import Header from '../components/Header';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
+import { createStore } from 'redux'; // deprecated 됐지만 그냥 쓰기
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      {/* @ts-expect-error */}
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        {/* @ts-expect-error */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
