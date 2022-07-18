0b4def6031e252461090e857a9d02b88927d22de
Bhanjo <hanjo1515@naver.com>
2022-07-18 15:54:38 +0900

    refactor: todo redux를 hooks를 이용해 리팩터링, memo 적용 최적화

--- containers/CounterContainer.tsx
+++ containers/CounterContainer.tsx
@@ -6,6 +6,7 @@ import Counter from '../pages/redux-test/Counter';
 import { useSelector, useDispatch } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
 import { useCallback } from 'react';
+import React from 'react';
 
 const CounterContainer = () => {
   // useSelector, useDispatch를 이용해 컨테이너 컴포넌트 만들기
@@ -25,7 +26,12 @@ const CounterContainer = () => {
   );
 };
 
-export default CounterContainer;
+// connect 사용 시 부모 컴포넌트가 리렌더링될 때
+// 해당 컨테이너 컴포넌트의 props가 바뀌지 않았다면
+// 리렌더링이 자동으로 방지되어 성능 최적화가 된다.
+// 하지만, useSelector의 경우 이 작업이 이루어 지지 않으므로
+// 성능 최적화를 위해서는 React.memo를 사용해 성능 최적화를 사용해야 한다.
+export default React.memo(CounterContainer);
 
 // *****************
 // 스토어 안의 상태를 컴포넌트 props로 넘겨주기 위해 사용
