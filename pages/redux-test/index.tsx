743c00dd42b59073b32b58e86025d722e8d2eee5
Bhanjo <hanjo1515@naver.com>
2022-07-18 15:58:36 +0900

    style: 안쓰는 props 삭제

--- pages/redux-test/index.tsx
+++ pages/redux-test/index.tsx
@@ -4,10 +4,8 @@ import TodosContainer from '../../containers/TodosContainer';
 const ReduxTest = () => {
   return (
     <div>
-      {/* @ts-expect-error */}
-      <CounterContainer number={0} />
+      <CounterContainer />
       <hr />
-      {/* @ts-expect-error */}
       <TodosContainer />
     </div>
   );
